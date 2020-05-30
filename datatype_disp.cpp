c3a04ae318a386d5b2fe97aa3fa079aad591300a
dwivediparas777 <dwivedi.paras777@gmail.com>
2020-05-30 20:12:16 +0530

    first c++ changes push

---
+++ datatype_disp.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  cout << "Size of boolean is :: " << sizeof(bool) << " bytes" << endl;
+  cout << "Size of char is :: " << sizeof(char) << " bytes" << endl;
+  cout << "Size of short int is :: " << sizeof(short int) << " bytes" << endl;
+  cout << "Size of unsigned short int is :: " << sizeof(unsigned short int) << " bytes" << endl;
+  cout << "Size of signed short int is :: " << sizeof(signed short int) << " bytes" << endl;
+  cout << "Size of int is :: " << sizeof(int) << " bytes" << endl;
+  cout << "Size of long int is :: " << sizeof(long int) << " bytes" << endl;
+  cout << "Size of float is :: " << sizeof(float) << " bytes" << endl;
+  cout << "Size of double is :: " << sizeof(double) << " bytes" << endl;
+  return 0;
+}
